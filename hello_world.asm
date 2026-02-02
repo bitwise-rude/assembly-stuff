ece689e457d84cda097540e7b2ab477f15814a8b
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-02-02 12:55:47 +0545

    hello world added

---
+++ hello_world.asm
@@ -0,0 +1,20 @@
+format ELF64 executable
+
+segment readable executable
+entry main
+
+main:
+    mov rax,1
+    mov rdi,1
+    mov rsi,msg
+    mov rdx,msg_len
+    syscall
+
+    mov rax,60
+    mov rdi,0
+    syscall
+
+segment readable writable
+msg db "Hello World", 10
+msg_len = $ - msg
+
